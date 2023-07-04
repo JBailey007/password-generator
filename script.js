58d7f97f908f03fc230ba8549843124e3f866c8c
Jalen Bailey <jwbailey13@gmail.com>
2023-07-04 10:02:44 -0600

    added if statements for each option and added the charset object to house all of the options

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var charset = {
+  num: "0123456789",
+  specialChar: "!@#$%&'()*+,^-./:;<=>?[]_`{~}|",
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+}
 
 function generatePassword() 
 // Length verification
 {
+  var passwordInfo = "";
   var length = window.prompt("Enter the desired password length (between 8 and 128 characters):");
   while (isNaN(length) || length < 8 || length > 128) {
     length = window.prompt("Invalid input! Please enter a length between 8 and 128 characters:");
@@ -12,55 +19,50 @@ function generatePassword()
   var chooseLowerCase = confirm("Would you like to include lower case letters?");
   if (chooseLowerCase) {
     window.alert("You have selected lowercase letters to be included")
+    passwordInfo = passwordInfo + charset.lowerCase; 
   } else {
     window.alert("Lowercase letters will NOT be included")
+    passwordInfo !== passwordInfo + charset.lowerCase; 
   }
   
 // Uppercase Option
   var chooseUpperCase = confirm("Would you like to include upper case letters?");
   if (chooseUpperCase) {
     window.alert("You have selected uppercase letters to be included")
+    passwordInfo = passwordInfo + charset.upperCase; 
   } else {
     window.alert("Uppercase letters will NOT be included")
+    passwordInfo !== passwordInfo + charset.upperCase; 
   }
+
 // Numeric Option
   var chooseNumeric = confirm("Would you like to include numbers?");
   if (chooseNumeric) {
     window.alert("You have selected numbers to be included")
+    passwordInfo = passwordInfo + charset.num; 
   } else {
     window.alert("Numbers will NOT be included")
+    passwordInfo !== passwordInfo + charset.num; 
   }
+
 // Special Character Option
   var chooseSpecial = confirm("Would you like to include special characters?");
   if (chooseSpecial) {
     window.alert("You have selected special characters to be included")
+    passwordInfo = passwordInfo + charset.specialChar; 
   } else {
     window.alert("Special characters will NOT be included")
+    passwordInfo !== passwordInfo + charset.specialChar; 
   }
+
 // For Loop for Random Password
-var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?";
-password1 = "";
-if (chooseLowerCase === true && chooseUpperCase === true && chooseNumeric === true && chooseSpecial === true) {
+var randomPassword = "";
+
   for ( var i = 0; i < length; i++) {
-    var randomIndex = Math.floor(Math.random() * charset.length);
-    password1 += charset.charAt(randomIndex);
-  }
-  return password1; 
-} else if (chooseLowerCase !== true && chooseUpperCase === true && chooseNumeric === true && chooseSpecial === true){
-
-} else if (chooseLowerCase !== true && chooseUpperCase !== true && chooseNumeric === true && chooseSpecial === true){
-
-} else if (chooseLowerCase !== true && chooseUpperCase !== true && chooseNumeric !== true && chooseSpecial === true){
-
-}else {
-
-}
-  for ( var i = 0; i < length; i++) {
-    var randomIndex = Math.floor(Math.random() * charset.length);
-    password += charset.charAt(randomIndex);
+    randomPassword += passwordInfo[Math.floor(Math.random() * passwordInfo.length)];
   }
 
-  return password; 
+  return randomPassword; 
 }
 
 // Write password to the #password input
@@ -69,31 +71,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// Series of Prompts for password Criteria
-// WHEN prompted for password criteria
-
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-
-
-
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-
-
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
